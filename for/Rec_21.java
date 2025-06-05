b37c94f0d163d75926a1f82547e2549dcba1aedf
gdov1012 <valerogiovanna7@gmail.com>
2025-06-05 20:16:48 -0300

    Add files via upload

---
+++ for/Rec_21.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package recuperacao_1;
+
+/**
+ *
+ * @author Admin
+ */
+public class Rec_21 {
+    public static void main(String[] args) {
+ 
+        for (int cc=1; cc<=10; cc++){
+            System.out.println(cc);
+        }
+    
+    }}
