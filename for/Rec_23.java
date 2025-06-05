1446660885e340407575b150907c2cc923fc09c0
gdov1012 <valerogiovanna7@gmail.com>
2025-06-05 20:36:56 -0300

    Add files via upload

---
+++ for/Rec_23.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package recuperacao_1;
+
+/**
+ *
+ * @author Admin
+ */
+public class Rec_23 {
+      public static void main(String[] args) {
+ 
+        for (int cc=1;cc<=50 ; cc++){
+            if (cc%2==0){
+                System.out.println(+cc+ " eh par");
+            }else{
+                System.out.println(+cc+ " eh impar");
+            }
+        }
+}}
