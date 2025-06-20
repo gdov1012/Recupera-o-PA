c952af0c6a5bfc4f569f527a40298bb22a311026
gdov1012 <valerogiovanna7@gmail.com>
2025-06-20 15:08:46 -0300

    Recuperacao_1.java

--- vetores/Recuperacao_1.java
+++ vetores/Recuperacao_1.java
@@ -1,20 +1,9 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
-package recuperacao_1;
+
 
 import java.util.Scanner;
 
-/**
- *
- * @author Admin
- */
 public class Recuperacao_1 {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         // TODO code application logic here
        Scanner scanner = new Scanner (System.in);
