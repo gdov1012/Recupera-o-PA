d0b550d10c9d9600e2eb8061611b0ee0e71ad93a
gdov1012 <valerogiovanna7@gmail.com>
2025-06-20 15:08:11 -0300

    Rec_2.java

--- vetores/Rec_2.java
+++ vetores/Rec_2.java
@@ -1,35 +1,20 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package recuperacao_1;
-
 import java.util.Scanner;
 
-/**
- *
- * @author Admin
- */
+
 public class Rec_2 {
-      public static void main(String[] args) {
-        // TODO code application logic here
-       Scanner scanner = new Scanner (System.in);
-       int n [][]  = new int [2] [2]; //matriz
-       
-       System.out.println("Digite um numero inteiro:");
-       for (int i = 0; i < n.length;i++){//linha
-           for (int a = 0; a< n[i].length; a++){//coluna
-               System.out.println("Posicao: ["+ i + " ]["+ a +"]"); //localizacao
-               n [i][a] = scanner.nextInt();//leitura 
-           }
-       }
-       System.out.println("Numeros fornecidos:");
-       for (int i = 0; i < n.length;i++){//linha
-           for (int a = 0; a< n[i].length; a++){//coluna
-           System.out.print(n[i][a] + "\t");
-           }
-           System.out.println();//fechamento for
-       }
-        scanner.close();//fechamento scanner
-           }}
+     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int[][] matriz2 = new int[2][2];
+        System.out.println("Digite os valores:");
+        for (int i = 0; i < 2; i++)
+            for (int j = 0; j < 2; j++)
+                matriz2[i][j] = scanner.nextInt();
+        System.out.println("Matriz:");
+        for (int[] linha : matriz2) {
+            for (int val : linha) System.out.print(val + " ");
+            System.out.println();
+        }
+    }
+    }
+
        
