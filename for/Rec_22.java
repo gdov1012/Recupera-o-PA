14da259d931b20fd68192d312e7852b1c863f898
gdov1012 <valerogiovanna7@gmail.com>
2025-06-20 16:42:58 -0300

    Rec_22.java

--- for/Rec_22.java
+++ for/Rec_22.java
@@ -1,15 +1,7 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package recuperacao_1;
 
 import java.util.Scanner;
 
-/**
- *
- * @author Admin
- */
+
 public class Rec_22 {
     public static void main(String[] args) {
          Scanner scanner = new Scanner (System.in);
